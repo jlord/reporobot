7128cd0c4c67da397f1c08b6da9782f73bde5e38
jlord <jessica.lord@github.com>
2014-01-02 19:19:00 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var hook = require('hubhook')();
+var http = require('http');
+
+module.exports = function() {
+  var server = http.createServer(handler)
+
+  function handler(req, res) {
+    if (req.method === 'POST' && req.url === '/push') {
+      return hook.handle(req, res);
+    }
+    res.statusCode = 404;
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify({
+      error: 404,
+      message: 'not_found',
+      hints: 'POST to /push'
+    }, true, 2));
+  }
+
+  hook.on('payload', function (payload) {
+    console.log(payload);
+  });
+  return server
+}
