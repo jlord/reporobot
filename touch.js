e952a2324860e4c22c91c52385d0ba155f59035c
jlord <jessica.lord@github.com>
2014-01-03 22:32:13 -0800

    need oauth

--- touch.js
+++ touch.js
@@ -1,6 +1,6 @@
+var Github = require('github-api')
+
 module.exports = function(object, request) {
-//  console.log("REQ", request)
-//  console.log("OBJ", object)
   
   getDetails(object)
 
@@ -12,7 +12,20 @@ module.exports = function(object, request) {
     details.repoURI = "https://www.github.com/" 
                   + details.username + "/" 
                   + details.repo + ".git"
-    console.log(details)
+    console.log("{red}" + details.username, "added you as a contributor.{/red}")
+    cloneRepo(details)
+  }
+  
+  function cloneRepo(details) {
+    var github = new Github({
+      type: "oauth",
+      token: ""
+    })
     
+    var repo = github.getRepo('jlord', 'reporobot')
+    
+    repo.write('master', 'hi.md', 'hello it is me', 'add file test', function(err) {
+      if (err) console.log(err)
+    }) 
   }
 }
