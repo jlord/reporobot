a7ec2070605e55f4816e6306ed5229b6de65cc13
jlord <jessica.lord@github.com>
2014-01-03 21:19:47 -0800

    create object w user, repo deets

--- touch.js
+++ touch.js
@@ -1,11 +1,18 @@
 module.exports = function(object, request) {
-  console.log("REQ", request)
-  console.log("OBJ", object)
+//  console.log("REQ", request)
+//  console.log("OBJ", object)
   
-  getUsername(object)
+  getDetails(object)
 
-  function getUsername(object) {
-    var subject = object.headers.Recieved.Subject
-    console.log(subject)
+  function getDetails(object) {
+    var subject = object.headers.Subject
+    var detailsArray = subject.split(" added you to ")
+    var details = { "username": detailsArray[0], 
+                    "repo": detailsArray[1] }
+    details.repoURI = "https://www.github.com/" 
+                  + details.username + "/" 
+                  + details.repo + ".git"
+    console.log(details)
+    
   }
 }
