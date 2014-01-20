7214a00a33026b76dfd097e675ba5dc5c52d0284
Jessica Lord <jessica.lord@github.com>
2014-01-20 15:05:52 -0800

    fix write contributors file

--- contributors.js
+++ contributors.js
@@ -2,13 +2,14 @@ var fs = require('fs')
 var file = 'contributors.json'
 
 module.exports = function(stats, callback) {
+  console.log("these are the stats", stats)
   
   fs.readFile(file, function (err, data) {
     if (err) console.log(err)
     
-    data = data.toString()
+    var oldData = data.toString()
     
-    if (data === "") var array = []
+    if (oldData === "") var array = []
     else var array = JSON.parse(data)
     
     array.push(stats)
@@ -19,7 +20,7 @@ module.exports = function(stats, callback) {
 function writeFile(file, array, callback) {
   fs.writeFile(file, array, function (err) {
     if (err) console.log(err)
-    console.log("Added" + stats.username + " to contributor file")
+    console.log("Added" + array[0].username + " to contributor file")
     callback()
   }) 
 }
