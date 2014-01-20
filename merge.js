2f2cf6d256c6d5e248c93910d8a326913e0fc72a
Jessica Lord <jessica.lord@github.com>
2014-01-20 14:25:17 -0800

    expected filename

--- merge.js
+++ merge.js
@@ -66,13 +66,14 @@ module.exports = function(pullreq, req) {
 function verifyFilename(prInfo) {
   // add /contributors/ to filename
   var filename = prInfo.filename
+  console.log(["filename from PR", filename])
   // if (filename.match('/contributors/test.md')) {
-  if (filename.match('/contributors/add-' + stats.username + '.md')) {
+  if (filename.match('contributors/add-' + stats.username + '.md')) {
     console.log("Filename: MATCH")
     verifyContent(prInfo)
   }
   else {
-    var message = 'Filename is different than expected: /contributors/add-' + stats.username 
+    var message = 'Filename is different than expected: contributors/add-' + stats.username 
     writeComment(message, stats.prNum)
   }
 }
