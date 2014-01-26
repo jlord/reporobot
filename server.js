b8f7086be4b1a325e0d87b47e066b16dbb97d28d
Jessica Lord <jessica.lord@github.com>
2014-01-25 19:13:33 -0800

    logging

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var bot = require("./index.js")
 var touch =  require("./email.js")
 var asciify = require('asciify')
-
+console.log("PENV", process.env)
 bot(touch).listen(process.env.PORT || 5563)
 asciify('reporobot', {font:'isometric2'}, function(err, res){ 
   if (err) console.log(err)
