2e99728f637555a1e1e2567a3359031da7788a6e
Jessica Lord <jessica.lord@github.com>
2014-01-26 17:01:23 -0800

    tests, template update

--- tests/test-multi-pr.js
+++ tests/test-multi-pr.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
-var pr = require('./tests/test-onbehalfof.js')
+var pr = require(__dirname + '/testonbehalfof.js')
 var accounts = ['jllord', 'goldenrod']
 var sourceAccount = 'jlord'
 
 accounts.forEach(function (account) {
+  console.log("Sending " + account + " on..")
   pr(sourceAccount, account)
 })
