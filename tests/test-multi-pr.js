e2a2b2d25d2692aa5b19e9547293f9b025eb4bca
Jessica Lord <jessica.lord@github.com>
2014-01-27 11:19:42 -0800

    cleanup

--- tests/test-multi-pr.js
+++ tests/test-multi-pr.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var pr = require(__dirname + '/testonbehalfof.js')
-var accounts = ['goldenrod']
+var pr = require(__dirname + '/test-onbehalfof.js')
+var accounts = ['jlord','goldenrod']
 var sourceAccount = 'jlord'
 
 accounts.forEach(function (account) {
