4ff3d3fb144eed9c12d73331e35bfba15116084b
Jessica Lord <jessica.lord@github.com>
2014-01-26 19:45:28 -0800

    que

--- index.js
+++ index.js
@@ -70,10 +70,15 @@ module.exports = function(onHook) {
       var pullreq = JSON.parse(buff)
       
       
-      var q = async.queue(mergePr(pullreq, function finishedPR(err, message) {
+      var q = async.queue(mergePr(pr, function finishedPR(err, message) {
         if (err) console.log([new Date(), message, err])
         console.log([new Date(), "Finished one PR"])
       }), 1)
+      
+      q.push(pullreq, function (err) {
+          if (err) console.log([new Date(), message, err]))
+          console.log([new Date(), "Finished this push"])
+      })
       
       q.saturated = function () { console.log("Queuing up")}
       
