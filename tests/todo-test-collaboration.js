b9cf910d47dff26980ba237f42d60b52459a87c3
Jessica Lord <jessica.lord@github.com>
2015-01-25 22:51:10 -0800

    laying out

--- tests/todo-test-collaboration.js
+++ tests/todo-test-collaboration.js
@@ -1,3 +1,23 @@
 // Reporobot should collaborate with ascii art on users' repos
 // when they add it as a collaborator. Test that it does actually
 // do this!
+
+var request = require('request')
+
+var emailData = require('./email-payload.json')
+
+var options = {
+  url: 'http://reporobot.jlord.us/push',
+  json: true,
+  body: emailData
+}
+
+request.post(options, function done(err, res, body) {
+  // check that reporobot edited
+  checkForCommit()
+})
+
+function checkForCommit() {
+  // fetch goldenrod/patchwork commits,
+  // see if reporobot did it in last minute
+}
