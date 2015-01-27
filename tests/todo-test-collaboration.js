5a901876afcb07fc5eb3b4503935ce553378ea53
Jessica Lord <jessica.lord@github.com>
2015-01-26 23:49:15 -0800

    yay passing test

--- tests/todo-test-collaboration.js
+++ tests/todo-test-collaboration.js
@@ -1,46 +1,56 @@
-// Reporobot should collaborate with ascii art on users' repos
-// when they add it as a collaborator. Test that it does actually
-// do this!
-
 var request = require('request')
 var tape = require('tape')
 
 var emailData = require('./email-payload.json')
 
-// var options = {
-//   url: 'http://reporobot.jlord.us/push',
-//   json: true,
-//   body: emailData }
-//
-// request.post(options, function done(err, res, body) {
-//   // check that reporobot edited
-//   checkForCommit()
-// })
+// send a sample email payload to RR's server to trigger
+// the contribution on test account, goldenrod. Then fetch
+// commits to verify RR commited.
 
-function checkForCommit() {
+tape("Test RR collaborates on users' repos", function(t) {
 
-  var timeframe = new Date(Date.now() - 20000)
   var options = {
-    headers: { 'User-Agent': 'request' },
-    url: 'https://api.github.com/repos/goldenrod/patchwork/commits',
+    url: 'http://reporobot.jlord.us/push',
     json: true,
-    qs: {
-      sha: 'add-goldenrod',
-      author: 'reporobot',
-      since: timeframe.toISOString()
-    }
+    body: emailData
   }
 
-  request(options, function fetchedCommit(err, res, body) {
-    if (err) return console.log(err)
-    if (res.statusCode !== 200) return console.log("ERROR", res.statusCode, body)
-    if (body.length < 1) return console.log("ERROR", "No commits")
-    readCommits(body)
+  // send a sample email payload to RR server
+  request.post(options, function done(err, res, body) {
+    if (err) {
+      t.error(err, "Error posting email payload")
+      return t.end()
+    }
+    setTimeout(checkForCommit(), 5000)
   })
-}
 
-function readCommits(commits) {
+  function checkForCommit() {
+    var timeframe = new Date(Date.now() - 20000)
+    var options = {
+      headers: { 'User-Agent': 'request' },
+      url: 'https://api.github.com/repos/goldenrod/patchwork/commits',
+      json: true,
+      qs: {
+        sha: 'add-goldenrod',
+        author: 'reporobot',
+        since: timeframe.toISOString()
+      }
+    }
 
-}
+    // Get commits made on the add-goldenrod branch, by RR in the lasts 20 seconds.
+    request(options, function fetchedCommit(err, res, body) {
+      if (err || res.statusCode !== 200) {
+        t.error(err || res.statusCode, "Error fetching commits")
+        return t.end()
+      }
+      if (body.length < 1) {
+        t.fail("No commit found.")
+        return t.end()
+      }
+      t.ok(body.length > 0, "A commit was made recently.")
+      t.end()
+    })
+  }
+})
 
-checkForCommit()
+// TODO verify content of commit
