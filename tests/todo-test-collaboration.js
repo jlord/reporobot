3d9a3944d79784afeea489331e2e89ec5920517b
Jessica Lord <jessica.lord@github.com>
2015-01-26 22:25:15 -0800

    fetch commits

--- tests/todo-test-collaboration.js
+++ tests/todo-test-collaboration.js
@@ -3,21 +3,44 @@
 // do this!
 
 var request = require('request')
+var tape = require('tape')
 
 var emailData = require('./email-payload.json')
 
-var options = {
-  url: 'http://reporobot.jlord.us/push',
-  json: true,
-  body: emailData
-}
-
-request.post(options, function done(err, res, body) {
-  // check that reporobot edited
-  checkForCommit()
-})
+// var options = {
+//   url: 'http://reporobot.jlord.us/push',
+//   json: true,
+//   body: emailData }
+//
+// request.post(options, function done(err, res, body) {
+//   // check that reporobot edited
+//   checkForCommit()
+// })
 
 function checkForCommit() {
-  // fetch goldenrod/patchwork commits,
-  // see if reporobot did it in last minute
+
+  var timeframe = new Date(Date.now() - 20000)
+  var options = {
+    headers: { 'User-Agent': 'request' },
+    url: 'https://api.github.com/repos/goldenrod/patchwork/commits',
+    json: true,
+    qs: {
+      sha: 'add-goldenrod',
+      author: 'reporobot',
+      since: timeframe.toISOString()
+    }
+  }
+
+  request(options, function fetchedCommit(err, res, body) {
+    if (err) return console.log(err)
+    if (res.statusCode !== 200) return console.log("ERROR", res.statusCode, body)
+    if (body.length < 1) return console.log("ERROR", "No commits")
+    readCommits(body)
+  })
 }
+
+function readCommits(commits) {
+
+}
+
+checkForCommit()
