522ec0bdf678b4bc3ac34753fc5dd9f7c5eeaeae
Jessica Lord <jlord@github.com>
2016-02-15 23:02:42 -0800

    Print message

--- rebuildpage.js
+++ rebuildpage.js
@@ -2,5 +2,6 @@ var rebuildpage = require('./buildpage.js')
 
 rebuildpage(function done(error, message){
   if (error) return console.log(error, message)
+  if (message) console.log(message)
   console.log("Finished rebuilding page.")
 })
