4c96aa494c798879a0dcb645a4f4f4e252c7fde0
Jessica Lord <jlord@github.com>
2016-08-31 17:03:49 -0700

    Modularize

--- accept-invites.js
+++ accept-invites.js
@@ -1,34 +1,43 @@
 var request = require('request')
 
-var options = {
-    url: 'https://api.github.com/user/repository_invitations',
-    json: true,
-    headers: { 'User-Agent': 'request',
-               'Authorization': 'token ' + process.env['REPOROBOT_TOKEN']
-    }
-}
+// When a new invite email is recieved this is
+// called and all of the pending invites are accepted
+module.exports = function acceptInvites (callback) {
+  var options = {
+      url: 'https://api.github.com/user/repository_invitations',
+      json: true,
+      headers: { 'User-Agent': 'request',
+                 'Authorization': 'token ' + process.env['REPOROBOT_TOKEN']
+      }
+  }
 
-get30Invites()
+  acceptBatch()
 
-function get30Invites () {
-  request(options, function getInvites (error, response, body) {
-    if (error) return console.log(error)
-    console.log(response.statusCode)
-    if (!error && response.statusCode == 200) {
-      console.log(body.length, 'invites')
-      body.forEach(function getID (invite) {
-        approveInvite(invite.id)
-      })
-    }
-  })
-}
+  function acceptBatch () {
+    request(options, function gotInvites (err, response, body) {
+      var inviteCount = 0
+      var acceptedCount = 0
 
-
-
-function approveInvite (inviteID) {
-  options.url = 'https://api.github.com/user/repository_invitations/' + inviteID
-  request.patch(options, function approved (error, response, body) {
-    if (error) return console.log(error)
-    console.log('invite', inviteID, response.statusCode)
-  })
+      if (err) return callback(err)
+      if (!err && response.statusCode == 200) {
+        // Return if there are no pending invites
+        inviteCount = body.lenth
+        if (inviteCount === 0) return callback()
+        console.log(new Date(), 'Accepting', body.length, 'invites')
+        body.forEach(function getID (invite) {
+          // Accept each invite
+          options.url = 'https://api.github.com/user/repository_invitations/' + inviteID
+          request.patch(options, function approved (err, response, body) {
+            if (err) return callback(err)
+            console.log(new Date(), 'Invite', body.repository.owner.login, body.id, response.statusCode)
+            acceptedCount++
+            // Once you've accepted 30, the batch max, try again
+            // to see if there are more, else you're done!
+            if (acceptedCount === 30) return acceptBatch()
+            if (acceptedCount === inviteCount) return callback()
+          })
+        })
+      }
+    })
+  }
 }
