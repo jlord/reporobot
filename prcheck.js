c410ed5a3cd9fd8941d67c9b4efb09d66766a6ab
Jessica Lord <jlord@github.com>
2016-09-02 16:09:22 -0700

    Cleanup

--- prcheck.js
+++ prcheck.js
@@ -5,18 +5,19 @@ var request = require('request')
 // Because PRs are merged so fast by @RR, we loop through each of the closed
 // issues to find the user's PR.
 // called by: checkPR(username, function(err, pr){ prStatus(res, err, pr) })
-// call back includes a function which sends the pr boolean on as a response
+// callback includes a function which sends the pr boolean on as a response
 
 module.exports = function (username, callback) {
-  var baseURL = 'https://api.github.com/repos/jlord/patchwork/issues?state=closed'
   var options = {
-    url: baseURL,
+    url: 'https://api.github.com/repos/jlord/patchwork/issues?state=closed',
     json: true,
     headers: {
       'User-Agent': 'request',
       'Authorization': 'token ' + process.env['REPOROBOT_TOKEN']
     }
   }
+
+  request(options, getIssues)
 
   function getIssues (error, response, body) {
     if (error) return callback(error, null)
@@ -35,9 +36,8 @@ module.exports = function (username, callback) {
         return callback(null, pr)
       }
     }
-    // why this here, shouldn't it fail early?
+    // TODO why this here, shouldn't it fail early?
+    // Check for non 200 responses
     callback(error, pr)
   }
-
-  request(options, getIssues)
 }
