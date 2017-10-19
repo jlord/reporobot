5feacf5f87976deb8e3d01c6aaf20c0325bf2c15
Jessica Lord <jlord@github.com>
2017-10-19 03:09:56 +0000

    Don't treat non 200s as errors

--- clearuser.js
+++ clearuser.js
@@ -49,8 +49,9 @@ module.exports = function deleteFile (username, callback) {
     }
     request.del(options, function (err, response, body) {
       if (err) return callback(err, 'Error with delete request')
-      // if (response.statusCode != 200) return callback(new Error('Non 200 for delete: ' + response.statusCode))
-      if (response.statusCode != 200) return callback(response, response.statusCode))
+      if (response.statusCode != 200) {
+        return callback(null, 'Non 200 for delete: ' + response.statusCode + ' ' + response.body.message)
+      }
       console.log(new Date(), 'Deleted ' + file.name)
       callback(null)
     })
