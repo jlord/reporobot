3917d0d592d401bac773d1c84a32f0e367856d79
Jessica Lord <jessica.lord@mongodb.com>
2017-11-01 16:34:28 -0400

    Poll API and accept invites

--- accept-invites.js
+++ accept-invites.js
@@ -1,10 +1,13 @@
 var request = require('request')
 
+var makeAscii = require('ascii.js')
+
 // TODO check an invite is to patchwork,
 // then accept invite, then draw the ascii art
 
-// When a new invite email is recieved this is
-// called and all of the pending invites are accepted
+// This file is run constantly, polling the GitHub API
+// and checking for new invites and accepting them.
+
 module.exports = function acceptInvites (callback) {
   var options = {
       url: 'https://api.github.com/user/repository_invitations',
@@ -26,18 +29,25 @@ module.exports = function acceptInvites (callback) {
 
   function acceptBatch () {
     request(options, function gotInvites (err, response, body) {
-      var acceptedCount = 0
+      var handledCount = 0
 
       if (err) return thunk(err)
       if (response.statusCode !== 200) {
-	console.log('Bad invite request', response.statusCode)
-	return thunk(new Error('Not a 200 response'))
+    	  console.log('Bad invite request', response.statusCode)
+    	  return thunk(new Error('Not a 200 response'))
       }
       // Return if there are no pending invites
       var inviteCount = body.length
       if (inviteCount === 0) return thunk()
+
       console.log(new Date(), 'Accepting', body.length, 'invites')
       body.forEach(function getID (invite) {
+        // Skip invites that aren't for the patchwork repo
+        if (invite.name !== 'patchwork') {
+          handledCount++
+          return
+        }
+
         var username = invite.inviter.login
         var inviteID = invite.id
         // Accept each invite
@@ -45,11 +55,14 @@ module.exports = function acceptInvites (callback) {
         request.patch(options, function accepted (err, response, body) {
           if (err) return thunk(err)
           console.log(new Date(), 'Accepted invite from', username, inviteID, response.statusCode)
-          acceptedCount++
+          handledCount++
+          // Now go contribute to the users project with ascii art
+          makeAscii(username, callback)
+
           // Once you've accepted 30, the batch max, try again
           // to see if there are more, else you're done!
-          if (acceptedCount === 30) return acceptBatch()
-          if (acceptedCount === inviteCount) return thunk()
+          if (handledCount === 30) return acceptBatch()
+          if (handledCount === inviteCount) return thunk()
         })
       })
     })
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-var server = require('./index.js')
 var asciify = require('asciify')
+
+var server = require('./index.js')
+var pollForInvites = require('accept-invites.js')
 
 var token = process.env['REPOROBOT_TOKEN']
 if (!token) throw new Error('Missing REPOROBOT_TOKEN')
 var contributors = process.env['CONTRIBUTORS']
 if (!contributors) throw new Error('Missing CONTRIBUTORS')
 
-// TODO set up interval
+setInterval(pollForInvites, 20000, function callback (err, msg) {
+  if (err) console.log(err, msg)
+})
 
 server().listen(process.env.PORT || 5563)
 asciify('reporobot', { font: 'isometric2' }, function startupArt (err, res) {
